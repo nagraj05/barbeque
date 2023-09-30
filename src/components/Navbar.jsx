52cf27c0cc086386767a8b4c553a4b5134aab995
unitech <ntadingi5@gmail.com>
2023-09-30 09:07:55 +0530

    added routes to menu and reviews

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
       <div className="flex justify-between items-center px-10 py-6 text-white bg-gray-800">
         <h1 className="text-xl font-bold font-poppins">The Food website</h1>
         <ul className="flex space-x-8 font-bold font-poppins cursor-pointer">
-          <li className="hover:text-red-500">Home</li>
-          <li className="hover:text-red-500">Menu</li>
-          <li className="hover:text-red-500">Reviews</li>
+          <li className="hover:text-red-500"><Link to="/">Home</Link></li>
+          <li className="hover:text-red-500"><Link to="/menu">Menu</Link></li>
+          <li className="hover:text-red-500"><Link to="/reviews">Reviews</Link></li>
         </ul>
         <div className=" flex items-center space-x-5 font-bold font-poppins">
           <button>Sign In</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import Navbar from "../components/Navbar"
 import Banner from "../components/Banner"
 import HappinessCard from "../components/HappinessCard"
 import MenuBanner from "../components/MenuBanner"
@@ -7,7 +6,6 @@ import Footer from "../components/Footer"
 export default function Home() {
   return (
     <div>
-    <Navbar />
     <Banner />
     <HappinessCard />
     <MenuBanner />
